41cd1c7b242e1bf8f4db687a798ea4092dae7c7f
Curator <rayhanchy576@gmail.com>
2025-06-21 17:37:01 +0600

    Create users-data.js

---
+++ users-data.js
@@ -0,0 +1,5 @@
+export default [
+  { name: "Elina Rahman", birth: "1997-03-21", role: "Dreamer", classesTaken: 5 },
+  { name: "Tanvir Hossain", birth: "1990-11-05", role: "Side Hustler", classesTaken: 2 },
+  { name: "Sarah Khan", birth: "1985-07-17", role: "Corporate Escaper", classesTaken: 9 }
+];
